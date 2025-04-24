ce68570d267210b67c3348fb2a563609fde17d65
mittal9999 <mittalrathva99@gmail.com>
2025-04-25 00:23:46 +0530

    added Truthy and Falsy value

---
+++ Section-04-Intro-Logic/Logica_Assignment/script.js
@@ -0,0 +1,35 @@
+//  = assigns the right side value only if the left is a falsy value.
+
+
+let a = null;
+if(!a)  {
+    a = 10;
+}
+
+//  a = a || 10;
+ a ||= 10;
+console.log(a);
+
+
+// && = assigns the right side value only if the left is a truthy value
+
+let b = 20;
+if(b){
+    b= 20;
+}
+
+b =b && 20;
+b &&= 20;
+console.log(b);
+
+// ?? = assigns the right side value only if the left is null or modifined
+
+
+let c = null;
+if( c === null || c ===undefined) {
+c =30;
+}
+
+c = c ?? 30;
+c ??= 30;
+console.log(c);
--- Section-04-Intro-Logic/Truthy-Falsy_value/script.js
+++ Section-04-Intro-Logic/Truthy-Falsy_value/script.js
@@ -1,5 +1,78 @@
-const email = 'test@test.com';
+// Truthy Value
+// -Everything else that is not falsy
+// -true
+// -'0'(0 in a string)
+// -''(space in a string)
+// -'false' (false in a string)
+// -[] (empty array)
+// -{} (empty object)
+// -function () { (empty function)}
 
-if(email){
-    console.log('You passed in an email');
+const y = function (){};
+if (y) {
+    console.log('This is truthy');
+} else {
+    console.log('This is falsy');
 }
+console.log(Boolean(y));
+
+
+
+// const email = 'test@test.com';
+// if (email) {
+//     console.log('You passed in an email');
+// }
+// console.log(Boolean(email));
+
+// Falsy Values:
+// -false
+// -0
+// -"" or '' (Empty string)
+// -null-undefined
+// -NaN
+
+const x = false;
+if (NaN) {
+    console.log('This is truthy');
+} else {
+    console.log('This is falsy');
+}
+console.log(Boolean(x));
+
+// Truthy and falsy caveats
+const  children = 3;
+
+if(!isNaN (children) ) {
+    console.log(`You have  ${children} children`);
+}else {
+    console.log('Please  enter number of children');
+}
+
+// Chekig for empty arrays
+// const posts = ['Post One' , 'Post Two'];
+const posts = ['Post One'];
+             
+if(posts.length > 0) {
+    console.log('List Posts');
+}else {
+    console.log('No Posts To List');
+}
+
+// Checking for empty objects
+const user = {
+    name :'Brad'
+};
+
+
+if(Object.keys(user).length > 0) {
+console.log('List user')
+}else {
+    console.log('No User');              
+}
+
+
+// Loose Equality (==)
+console.log('' === 0);
+console.log(0 === 0);
+console.log(null === undefined);
+
